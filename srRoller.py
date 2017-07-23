b9f771508e67d923a7a9df9cb5481b6af65e9aec
Andrew Miesem <andrew.miesem@gmail.com>
2017-07-22 20:47:33 -0600

    basic GUI construction started

--- srRoller.py
+++ srRoller.py
@@ -7,7 +7,7 @@ performing quick success tests, with or without the Edge-related Rule of Six
 This relies on the SuccessTest class in the dice module."""
 
 #standard library modules
-import tkinter as TK
+import tkinter as tk
 #custom module
 from dice.successtest import SuccessTest
 
@@ -21,5 +21,33 @@ __maintainer__ = "Andrew Miesem"
 __email__ = "andrew.miesem@gmail.com"
 __status__ = "Development"
 
-st = SuccessTest
-tk = TK.Tk
+def placeholder(): print('placeholder event')
+file_reset = placeholder
+
+st = SuccessTest()
+root = tk.Tk()
+root.geometry('500x300')
+root.resizable(width=False, height=True)
+root.title('Shadowrun Dice Roller')
+
+#application menu
+menubar = tk.Menu(root)
+root.config(menu=menubar)
+
+filemenu = tk.Menu(menubar)
+filemenu.add_command(label='Reset', command=file_reset)
+filemenu.add_command(label='Exit', command=root.destroy)
+
+menubar.add_cascade(label="File", menu=filemenu)
+
+#application fields
+tk.Label(root, text="Dice Pool").grid(row=0, sticky=tk.NE)
+tk.Label(root, text="Threshold").grid(row=1, sticky=tk.NE)
+
+pool = tk.Entry(root)
+threshold = tk.Entry(root)
+
+pool.grid(row=0,column=1)
+threshold.grid(row=1,column=1)
+
+root.mainloop()
